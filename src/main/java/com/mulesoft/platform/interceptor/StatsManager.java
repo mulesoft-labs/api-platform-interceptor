99227be1088f0176d40f88e334bbaba11505c2b0
santiagopurucker <santiago.purucker@mulesoft.com>
2017-02-15 22:00:30 -0300

    Adding strategies

--- src/main/java/com/mulesoft/platform/interceptor/StatsManager.java
+++ src/main/java/com/mulesoft/platform/interceptor/StatsManager.java
@@ -2,21 +2,21 @@ package com.mulesoft.platform.interceptor;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import com.google.gson.JsonObject;
 
 public class StatsManager {
 	
-	private enum Strategy {NONE} ;
+	private enum Strategy {NONE, FAILS_VS_OKS, PORCENTAGE} ;
 	private enum HttpVerb {POST,GET, NONE};
 	private int statusCode;
 	private String payload;
 	private Strategy strategy;
-	private Map<Integer, Integer> history;
 	private List<String> pathIntercepted;
 	private HttpVerb httpVerb;
-	//final String strategy;
+	private int qtyToFail;
+	private int failureAllowed;
+	private int porcentage;
 	
 	public StatsManager(JsonObject jsonObject) {	
 		this.statusCode = jsonObject.get("statusCode").getAsInt();
@@ -25,6 +25,9 @@ public class StatsManager {
 		String pathI=jsonObject.get("pathIntercepted")==null?null:jsonObject.get("pathIntercepted").getAsString();
 		this.pathIntercepted = Arrays.asList(pathI.split(","));
 		this.httpVerb = jsonObject.get("httpVerb")==null?HttpVerb.NONE:HttpVerb.valueOf(jsonObject.get("httpVerb").getAsString());
+		this.failureAllowed = jsonObject.get("failureAllowed")!=null?jsonObject.get("failureAllowed").getAsInt():0;
+		this.porcentage = jsonObject.get("porcentage")!=null?jsonObject.get("porcentage").getAsInt():0;
+		this.qtyToFail= failureAllowed;
 	}
 
 	public ResponseDetail getResponseDetail(String path, String httpVerb) {
@@ -33,7 +36,7 @@ public class StatsManager {
 		}
 		if(strategy == Strategy.NONE)
 			return new ProcessResponseDetail(statusCode, payload, validatePath(path) && this.validateHttpVerb());
-		return resolveStrategy();
+		return new ProcessResponseDetail(statusCode, payload, resolveStrategy());
 	}
 	
 	private boolean validateHttpVerb(){
@@ -51,8 +54,24 @@ public class StatsManager {
 		return true;
 	}
 	
-	private ResponseDetail resolveStrategy(){
-//		Math m = Math.;
-		return null;
+	private boolean resolveStrategy(){
+		if(this.strategy.equals(Strategy.FAILS_VS_OKS))
+			return failVsOks();
+		else if (this.strategy.equals(Strategy.PORCENTAGE))
+			return porcentageFailing();
+		throw new RuntimeException();
+	}
+	
+	private boolean failVsOks(){
+		if((this.qtyToFail--)>0)
+			return true;
+		this.qtyToFail= this.failureAllowed;
+		return false;
+	}
+	
+	private boolean porcentageFailing(){
+		if(Math.random()*100>porcentage)
+			return true;
+		return false;
 	}
 }
